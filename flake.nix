dc7f05a30e60ae5bd0964e89520e68932e7aa0a9
Big Boss <bigboss@metalrodeo.xyz>
2026-03-28 13:07:36 -0500

    chore: update nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "TypeScript project template";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
